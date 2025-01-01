b43fa1fa75aee17d4aebf36c380b4c2b8ce21d27
Bachtran301 <bachtranduy2003@gmail.com>
2025-01-01 11:16:53 +0700

    update

--- src/logo.js
+++ src/logo.js
@@ -9,5 +9,6 @@ function printLogo(){
             `.cyan);
     console.log('[+] Welcome & Enjoy Sir !'.green);
     console.log('[+] 📣 Error? - Update - New 📣  [https://t.me/+mjpRu3KiQTI4YmRl]'.blue);
+    console.log('[+] Version 0.1.1' .red);
 }
 module.exports = printLogo;
